053ecb84f7431a2c5856cb7784f41170f1f14cc0
AlexanderDong09 <alexanderdong09@gmail.com>
2024-11-05 22:32:48 -0500

    wjsh

--- app/JS/DOM.js
+++ app/JS/DOM.js
@@ -2,10 +2,10 @@ const DOMSelectors = {
   cardHeader: document.querySelector(".header"),
   container: document.querySelector(".container"),
   allButton: document.querySelector(".btn"),
-  galaxyButton: document.querySelector(".btn-gal"),
-  nebulaButton: document.querySelector(".btn"),
-  starsButton: document.querySelector(".btn"),
-  othersButton: document.querySelector(".btn"),
+  galaxyButton: document.querySelector(".gal"),
+  nebulaButton: document.querySelector(".neb"),
+  starsButton: document.querySelector(".star"),
+  blackHoleButton: document.querySelector(".bh"),
 };
 
 export { DOMSelectors };
--- app/JS/main.js
+++ app/JS/main.js
@@ -2,27 +2,7 @@ import "../CSS/style.css";
 import { DOMSelectors } from "./DOM.js";
 import { astronomyObjects } from "./objects.js";
 
-astronomyObjects.forEach((object) => {
-  const card = `
-    <div class="card">
-        <h3 class="header">${object.name}</h3>
-        <img src="${object.imageURL}" alt="${object.altText}">
-        <h4>Type of Object: ${object.type}</h4>
-        <h5>Distance to Earth (in light-years): ${object.distanceLightYears}</h5>
-        <h6>Discovery Year of Object: ${object.discoveryYear}</h6>
-        <h6>Fun Fact!: ${object.name} is ${object.notableFeatures}</h6>
-    </div>
-  `;
-
-  DOMSelectors.container.insertAdjacentHTML("beforeend", card);
-});
-
-DOMSelectors.allButton.addEventListener("click", function (event) {
-  event.preventDefault();
-  const clear = ``;
-
-  DOMSelectors.container.innerHTML = clear;
-
+function baseInsertion() {
   astronomyObjects.forEach((object) => {
     const card = `
       <div class="card">
@@ -37,16 +17,22 @@ DOMSelectors.allButton.addEventListener("click", function (event) {
 
     DOMSelectors.container.insertAdjacentHTML("beforeend", card);
   });
-});
+}
 
-DOMSelectors.galaxyButton.addEventListener("click", function (event) {
+baseInsertion();
+
+DOMSelectors.allButton.addEventListener("click", function (event) {
   event.preventDefault();
   const clear = ``;
 
   DOMSelectors.container.innerHTML = clear;
 
+  baseInsertion();
+});
+
+function specializedInsertion(type) {
   astronomyObjects
-    .filter((object) => object.type == "Galaxy")
+    .filter((object) => object.type == type)
     .forEach((object) => {
       const card = `
       <div class="card">
@@ -61,4 +47,36 @@ DOMSelectors.galaxyButton.addEventListener("click", function (event) {
 
       DOMSelectors.container.insertAdjacentHTML("beforeend", card);
     });
+}
+
+DOMSelectors.galaxyButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  const clear = ``;
+  DOMSelectors.container.innerHTML = clear;
+
+  specializedInsertion("Galaxy");
+});
+
+DOMSelectors.nebulaButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  const clear = ``;
+  DOMSelectors.container.innerHTML = clear;
+
+  specializedInsertion("Nebula");
+});
+
+DOMSelectors.starsButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  const clear = ``;
+  DOMSelectors.container.innerHTML = clear;
+
+  specializedInsertion("Star");
+});
+
+DOMSelectors.blackHoleButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  const clear = ``;
+  DOMSelectors.container.innerHTML = clear;
+
+  specializedInsertion("Black Hole");
 });
